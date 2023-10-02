0300d2f3f4e20e824ee886130831f6066e7db36b
Pastel Caique <caiquepericles.23@gmail.com>
2023-10-02 09:26:10 -0300

    Melhorei os nomes das constraints e também a estrutura

--- hermezBD.sql
+++ hermezBD.sql
@@ -8,14 +8,14 @@ use hermezBD;
 ////////////////////
 */
 create table Empresa(
-emp_cod int primary key,
+emp_cod int auto_increment primary key,
 emp_nome varchar(20) not null,
 emp_cnpj varchar(30) not null,
 emp_senha varchar(29) not null
 );
 
 create table Funcionario(
-fun_cod int primary key,
+fun_cod int auto_increment primary key,
 fun_nome varchar(30) not null,
 fun_funcao varchar(15) not null,
 fun_email varchar(30) not null,
@@ -23,11 +23,13 @@ fun_celular varchar(12) not null,
 fun_senha varchar(29) not null,
 emp_cod int,
 foreign key(emp_cod) references Empresa(emp_cod)
+-- key emp_cod(emp_cod)
 );
 
 create table Administrador(
 adm_cod int,
 foreign key(adm_cod) references Funcionario(fun_cod),
+-- key adm_cod(fun_cod)
 primary key(adm_cod),
 adm_senha varchar(29)
 );
@@ -43,11 +45,11 @@ create table Status(
 sta_cod int primary key,
 sta_nome varchar(15) not null,
 sta_valor int 
-constraint sta_valor_umaseis CHECK(sta_valor between 1 and 6)
+constraint sta_valor_um_a_seis CHECK(sta_valor between 1 and 6)
 );
 
 create table Chamado(
-cha_cod int primary key,
+cha_cod int auto_increment primary key,
 cha_desc varchar(77) not null,
 cha_dataInicio datetime not null,
 cha_dataFim datetime,
@@ -55,11 +57,11 @@ cha_local varchar(20),
 cha_titulo varchar(25),
 cha_tipoServ varchar(11),
 cha_prioridade int,
-constraint cha_prio_umaquatro CHECK(cha_prioridade between 1 and 4),
 fun_cod int,
-foreign key (fun_cod) references Funcionario(fun_cod),
 sta_cod int,
-foreign key(sta_cod) references Status(sta_cod)
+tec_cod int,
+constraint cha_prio_um_a_quatro CHECK(cha_prioridade between 1 and 4),
+foreign key (fun_cod) references Funcionario(fun_cod),
+foreign key(sta_cod) references Status(sta_cod),
+foreign key (tec_cod) references Tecnico(tec_cod)
 );
-Alter table Chamado add tec_cod int;
-alter table Chamado add foreign key (tec_cod) references Tecnico(tec_cod);
